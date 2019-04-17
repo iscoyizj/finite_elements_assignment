fb86d37391dd29a78d818dee612ccca4705b27b9
bkf16 <bkf16@mails.tsinghua.edu.cn>
2019-04-17 23:07:52 +0800

    Update Bar.cpp

--- src/cpp/Bar.cpp
+++ src/cpp/Bar.cpp
@@ -1,14 +1,4 @@
-/*****************************************************************************/
-/*  STAP++ : A C++ FEM code sharing the same input data file with STAP90     */
-/*     Computational Dynamics Laboratory                                     */
-/*     School of Aerospace Engineering, Tsinghua University                  */
-/*                                                                           */
-/*     Release 1.11, November 22, 2017                                       */
-/*                                                                           */
-/*     http://www.comdyn.cn/                                                 */
-/*****************************************************************************/
-
-#include "Bar.h"
+#include "4Q.h"
 
 #include <iostream>
 #include <iomanip>
@@ -17,22 +7,20 @@
 using namespace std;
 
 //	Constructor
-CBar::CBar()
+C4Q::C4Q()
 {
-	NEN_ = 2;	// Each element has 2 nodes
-	nodes_ = new CNode*[NEN_];
-    
+	NEN_ = 4;    // Each element has 4 nodes
+	nodes_=new CNode*[NEN_];
+
     ND_ = 6;
     LocationMatrix_ = new unsigned int[ND_];
 
 	ElementMaterial_ = nullptr;
 }
-
 //	Desconstructor
 CBar::~CBar()
 {
 }
-
 //	Read element data from stream Input
 bool CBar::Read(ifstream& Input, unsigned int Ele, CMaterial* MaterialSets, CNode* NodeList)
 {
@@ -50,23 +38,22 @@ bool CBar::Read(ifstream& Input, unsigned int Ele, CMaterial* MaterialSets, CNod
 	}
 
 	unsigned int MSet;	// Material property set number
-	unsigned int N1, N2;	// Left node number and right node number
+	unsigned int N1, N2,N3,N4;	// The first node from lower left, and the order is counterclock
 
-	Input >> N1 >> N2 >> MSet;
+	Input >> N1 >> N2 >> N3 >> N4 >> MSet;
     ElementMaterial_ = dynamic_cast<CBarMaterial*>(MaterialSets) + MSet - 1;
 	nodes_[0] = &NodeList[N1 - 1];
 	nodes_[1] = &NodeList[N2 - 1];
-
+	nodes_[2] = &NodeList[N3 - 1];
+	nodes_[3] = &NodeList[N4 - 1];
 	return true;
 }
-
 //	Write element data to stream
 void CBar::Write(COutputter& output, unsigned int Ele)
 {
 	output << setw(5) << Ele+1 << setw(11) << nodes_[0]->NodeNumber
-		   << setw(9) << nodes_[1]->NodeNumber << setw(12) << ElementMaterial_->nset << endl;
+		   << setw(9) << nodes_[1]->NodeNumber << setw(9) << nodes_[2]->NodeNumber << setw(9) << nodes_[3]->NodeNumber << setw(12) << ElementMaterial_->nset << endl;
 }
-
 //  Generate location matrix: the global equation number that corresponding to each DOF of the element
 //	Caution:  Equation number is numbered from 1 !
 void CBar::GenerateLocationMatrix()
@@ -76,88 +63,104 @@ void CBar::GenerateLocationMatrix()
         for (unsigned int D = 0; D < 3; D++)
             LocationMatrix_[i++] = nodes_[N]->bcode[D];
 }
-
 //	Return the size of the element stiffness matrix (stored as an array column by column)
-//	For 2 node bar element, element stiffness is a 6x6 matrix, whose upper triangular part
-//	has 21 elements
-unsigned int CBar::SizeOfStiffnessMatrix() { return 21; }
-
-//	Calculate element stiffness matrix 
-//	Upper triangular matrix, stored as an array column by colum starting from the diagonal element
-void CBar::ElementStiffness(double* Matrix)
+//	For 4 node bar element, element stiffness is a 6x6 matrix, whose upper triangular part
+//	has 36 elements
+unsigned int CBar::SizeOfStiffnessMatrix() { return 36; }
+double * CBar::NMatrix(double eta,double psi)
 {
-	clear(Matrix, SizeOfStiffnessMatrix());
-
-//	Calculate bar length
-	double DX[3];		//	dx = x2-x1, dy = y2-y1, dz = z2-z1
-	for (unsigned int i = 0; i < 3; i++)
-		DX[i] = nodes_[1]->XYZ[i] - nodes_[0]->XYZ[i];
-
-	double DX2[6];	//  Quadratic polynomial (dx^2, dy^2, dz^2, dx*dy, dy*dz, dx*dz)
-	DX2[0] = DX[0] * DX[0];
-	DX2[1] = DX[1] * DX[1];
-	DX2[2] = DX[2] * DX[2];
-	DX2[3] = DX[0] * DX[1];
-	DX2[4] = DX[1] * DX[2];
-	DX2[5] = DX[0] * DX[2];
-
-	double L2 = DX2[0] + DX2[1] + DX2[2];
-	double L = sqrt(L2);
-
-//	Calculate element stiffness matrix
-
-	CBarMaterial* material_ = dynamic_cast<CBarMaterial*>(ElementMaterial_);	// Pointer to material of the element
-
-	double k = material_->E * material_->Area / L / L2;
-
-	Matrix[0] = k*DX2[0];
-	Matrix[1] = k*DX2[1];
-	Matrix[2] = k*DX2[3];
-	Matrix[3] = k*DX2[2];
-	Matrix[4] = k*DX2[4];
-	Matrix[5] = k*DX2[5];
-	Matrix[6] = k*DX2[0];
-	Matrix[7] = -k*DX2[5];
-	Matrix[8] = -k*DX2[3];
-	Matrix[9] = -k*DX2[0];
-	Matrix[10] = k*DX2[1];
-	Matrix[11] = k*DX2[3];
-	Matrix[12] = -k*DX2[4];
-	Matrix[13] = -k*DX2[1];
-	Matrix[14] = -k*DX2[3];
-	Matrix[15] = k*DX2[2];
-	Matrix[16] = k*DX2[4];
-	Matrix[17] = k*DX2[5];
-	Matrix[18] = -k*DX2[2];
-	Matrix[19] = -k*DX2[4];
-	Matrix[20] = -k*DX2[5];
+	double * N;
+	N[0] = 0.25*(1-psi)*(1-eta); 
+	N[1] = 0.25*(1+psi)*(1-eta); 
+	N[2] = 0.25*(1+psi)*(1+eta); 
+	N[3] = 0.25*(1-psi)*(1+eta);
+	return N;
 }
-
-//	Calculate element stress 
-void CBar::ElementStress(double* stress, double* Displacement)
-{
-	CBarMaterial* material_ = dynamic_cast<CBarMaterial*>(ElementMaterial_);	// Pointer to material of the element
-
-	double DX[3];	//	dx = x2-x1, dy = y2-y1, dz = z2-z1
-	double L2 = 0;	//	Square of bar length (L^2)
-
-	for (unsigned int i = 0; i < 3; i++)
-	{
-		DX[i] = nodes_[1]->XYZ[i] - nodes_[0]->XYZ[i];
-		L2 = L2 + DX[i]*DX[i];
+void C4Q::ElementStiffness(double* Matrix){
+	clear(Matrix, SizeOfStiffnessMatrix());
+	C4QMaterial* material_ = dynamic_cast<C4QMaterial*>(ElementMaterial_);
+	E1=material_->E;
+	poisson1=material_->poisson;
+	double  B[8];
+	double  GN[8];
+	double	J[4];
+	double detj;
+	double inv_j[4];
+	double D[3];
+	int ngp=6;
+	double gp[ngp]=[-0.9324695142,-0.6612093865,-0.2386191861,0.2386191861,0.6612093865,0.9324695142];
+    double w[ngp]=[0.1713244924,0.3607615730,0.4679139346,0.4679139346,0.3607615730,0.1713244924];
+	D[0]=E1/(1-poisson1*poisson1);
+	D[1]=poisson1*E1/(1-poisson1*poisson1);
+	D[2]=E1/(2*(1+poisson1));
+	for(unsigned int i=0,i++,i<ngp){ 
+   		for(unsigned int j=0,j++,j<ngp){
+    		eta = gp(i);             
+    		psi = gp(j);
+			GN[0]=0.25*(eta-1);
+			GN[1]=0.25*(1-eta);
+			GN[2]=0.25*(eta+1);
+			GN[3]=0.25*(-eta-1);
+			GN[4]=0.25*(psi-1);
+			GN[5]=0.25*(-psi-1);
+			GN[6]=0.25*(psi+1);
+			GN[7]=0.25*(1-psi);
+			for (unsigned int i=0; i < 4; i++){
+				J[i]=0;
+			}
+			for (unsigned int j = 0; j < 4; j++){
+						J[0]+=GN[j]*nodes_[j]->XYZ[0];
+						J[1]+=GN[j]*nodes_[j]->XYZ[1];
+						J[2]+=GN[j + 4]*nodes_[j]->XYZ[0];
+						J[3]+=GN[j + 4]*nodes_[j]->XYZ[1];
+			}
+			detj=J[0]*J[3]-J[1]*J[2];
+			inv_j[0]=J[3]/detJ;
+			inv_j[1]=-J[1]/detJ;
+			inv_j[2]=-J[2]/detJ;
+			inv_j[3]=J[0]/detJ;
+			for (unsigned int i=0; i < 2; i++){
+				for (unsigned int j = 0; j < 4; j++){
+					B[4*i+j]=inv_j[2*i]*GN[j]+inv_j[2*i+1]*GN[4+j];
+				}
+			}
+			Matrix[0]+=w[i]*w[j]*(B[0]*D[0]*B[0] + B[4]*D[2]*B[4])*fabs(detj);
+			Matrix[1]+=w[i]*w[j]*(B[0]*D[0]*B[0] + B[4]*D[2]*B[4])*fabs(detj);
+			Matrix[2]+=w[i]*w[j]*(B[0]*D[1]*B[4] + B[4]*D[2]*B[0])*fabs(detj);
+			Matrix[3]+=w[i]*w[j]*(B[0]*D[1]*B[5] + B[4]*D[2]*B[1])*fabs(detj);
+			Matrix[4]+=w[i]*w[j]*(B[0]*D[0]*B[3] + B[4]*D[2]*B[7])*fabs(detj);
+			Matrix[5]+=w[i]*w[j]*(B[4]*D[1]*B[1] + B[0]*D[2]*B[5])*fabs(detj);
+			Matrix[6]+=w[i]*w[j]*(B[0]*D[2]*B[3] + B[4]*D[0]*B[7])*fabs(detj);
+			Matrix[7]+=w[i]*w[j]*(B[1]*D[1]*B[6] + B[5]*D[2]*B[2])*fabs(detj);
+			Matrix[8]+=w[i]*w[j]*(B[0]*D[1]*B[4] + B[4]*D[2]*B[0])*fabs(detj);
+			Matrix[9]+=w[i]*w[j]*(B[0]*D[0]*B[1] + B[4]*D[2]*B[5])*fabs(detj);
+			Matrix[10]+=w[i]*w[j]*(B[0]*D[0]*B[2] + B[4]*D[2]*B[6])*fabs(detj);
+			Matrix[11]+=w[i]*w[j]*(B[0]*D[1]*B[7] + B[4]*D[2]*B[3])*fabs(detj);
+			Matrix[12]+=w[i]*w[j]*(B[0]*D[2]*B[1] + B[4]*D[0]*B[5])*fabs(detj);
+			Matrix[13]+=w[i]*w[j]*(B[1]*D[0]*B[0] + B[5]*D[2]*B[4])*fabs(detj);
+			Matrix[14]+=w[i]*w[j]*(B[1]*D[0]*B[3] + B[5]*D[2]*B[7])*fabs(detj);
+			Matrix[15]+=w[i]*w[j]*(B[0]*D[1]*B[5] + B[4]*D[2]*B[1])*fabs(detj);
+			Matrix[16]+=w[i]*w[j]*(B[0]*D[1]*B[6] + B[4]*D[2]*B[2])*fabs(detj);
+			Matrix[17]+=w[i]*w[j]*(B[4]*D[1]*B[0] + B[0]*D[2]*B[4])*fabs(detj);
+			Matrix[18]+=w[i]*w[j]*(B[4]*D[1]*B[2] + B[0]*D[2]*B[6])*fabs(detj);
+			Matrix[19]+=w[i]*w[j]*(B[1]*D[1]*B[4] + B[5]*D[2]*B[0])*fabs(detj);
+			Matrix[20]+=w[i]*w[j]*(B[1]*D[1]*B[7] + B[5]*D[2]*B[3])*fabs(detj);
+			Matrix[21]+=w[i]*w[j]*(B[0]*D[0]*B[3] + B[4]*D[2]*B[7])*fabs(detj);
+			Matrix[22]+=w[i]*w[j]*(B[0]*D[2]*B[0] + B[4]*D[0]*B[4])*fabs(detj);
+			Matrix[23]+=w[i]*w[j]*(B[0]*D[2]*B[2] + B[4]*D[0]*B[6])*fabs(detj);
+			Matrix[24]+=w[i]*w[j]*(B[1]*D[0]*B[1] + B[5]*D[2]*B[5])*fabs(detj);
+			Matrix[25]+=w[i]*w[j]*(B[5]*D[1]*B[0] + B[1]*D[2]*B[4])*fabs(detj);
+			Matrix[26]+=w[i]*w[j]*(B[4]*D[1]*B[1] + B[0]*D[2]*B[5])*fabs(detj);
+			Matrix[27]+=w[i]*w[j]*(B[4]*D[1]*B[3] + B[0]*D[2]*B[7])*fabs(detj);
+			Matrix[28]+=w[i]*w[j]*(B[1]*D[1]*B[5] + B[5]*D[2]*B[1])*fabs(detj);
+			Matrix[29]+=w[i]*w[j]*(B[1]*D[2]*B[0] + B[5]*D[0]*B[4])*fabs(detj);
+			Matrix[30]+=w[i]*w[j]*(B[0]*D[2]*B[3] + B[4]*D[0]*B[7])*fabs(detj);
+			Matrix[31]+=w[i]*w[j]*(B[1]*D[0]*B[2] + B[5]*D[2]*B[6])*fabs(detj);
+			Matrix[32]+=w[i]*w[j]*(B[5]*D[1]*B[1] + B[1]*D[2]*B[5])*fabs(detj);
+			Matrix[33]+=w[i]*w[j]*(B[1]*D[1]*B[6] + B[5]*D[2]*B[2])*fabs(detj);
+			Matrix[34]+=w[i]*w[j]*(B[1]*D[2]*B[1] + B[5]*D[0]*B[5])*fabs(detj);
+			Matrix[35]+=w[i]*w[j]*(B[5]*D[1]*B[2] + B[1]*D[2]*B[6])*fabs(detj);
+		}
 	}
 
-	double S[6];
-	for (unsigned int i = 0; i < 3; i++)
-	{
-		S[i] = -DX[i] * material_->E / L2;
-		S[i+3] = -S[i];
-	}
-	
-	*stress = 0.0;
-	for (unsigned int i = 0; i < 6; i++)
-	{
-		if (LocationMatrix_[i])
-			*stress += S[i] * Displacement[LocationMatrix_[i]-1];
-	}
 }
