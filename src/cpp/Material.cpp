507d3d1fa4a5b5af2f81d51c172fbdb54a6c69a2
YifengZhang1122 <48181643+YifengZhang1122@users.noreply.github.com>
2019-06-19 18:28:35 +0800

    Add files via upload

--- src/cpp/Material.cpp
+++ src/cpp/Material.cpp
@@ -172,6 +172,24 @@ void CShellMaterial::Write(COutputter& output, unsigned int mset){
 	output << setw(5) << mset+1 << setw(16) << E << setw(16) << nu << setw(16) << density << setw(16)<< thick << endl;
 }
 
+bool CLinkMaterial::Read(ifstream& Input, unsigned int mset){
+	Input >> nset;	// Number of property set;
+  if (nset != mset + 1)
+	{
+		cerr << "*** Error *** Material sets must be inputted in order !" << endl
+			<< "    Expected set : " << mset + 1 << endl
+			<< "    Provided set : " << nset << endl;
+
+		return false;
+	}
+
+	Input>>k;
+	return true;
+}
+
+void CLinkMaterial::Write(COutputter& output, unsigned int mset){
+	output << setw(5) << mset+1 << setw(16) << k<< endl;
+}
 
 //	Write material data to Stream
 void CInfiMaterial::Write(COutputter& output, unsigned int mset)
